6642129d3a0c8f99774389778320bc8412145ad8
zykzhangyukang <3053161401@qq.com>
2020-08-16 22:54:48 +0800

    更新代码

--- src/main/java/com/coderman/web/UserController.java
+++ src/main/java/com/coderman/web/UserController.java
@@ -6,6 +6,10 @@ import com.coderman.model.User;
 import com.coderman.service.UserService;
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.mgt.eis.EnterpriseCacheSessionDAO;
+import org.apache.shiro.session.mgt.eis.SessionDAO;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +28,7 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
 
     @RequestMapping(value = "/login.do",method = RequestMethod.POST)
     @ResponseBody
