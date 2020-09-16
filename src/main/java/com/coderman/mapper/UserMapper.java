a6a334e28bb1402c243805acdfebec1f722cd54f
zykzhangyukang <3053161401@qq.com>
2020-09-16 09:37:20 +0800

    更新部分依赖

--- src/main/java/com/coderman/mapper/UserMapper.java
+++ src/main/java/com/coderman/mapper/UserMapper.java
@@ -6,16 +6,10 @@ import java.util.List;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
-
     int insert(User record);
-
     int insertSelective(User record);
-
     User selectByPrimaryKey(Integer id);
-
     int updateByPrimaryKeySelective(User record);
-
     int updateByPrimaryKey(User record);
-
     List<User> listAll();
 }
